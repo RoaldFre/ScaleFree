c5d29b0fd3bed6af28de79cf21d04f2fb496cdad
Roald Frederickx <roald.frederickx@gmail.com>
2012-05-09 22:05:17 +0200

    plotAllEquilibriumCoopPerParameters

---
+++ scripts/plotAllEquilibriumCoopPerParameters.m
@@ -0,0 +1,8 @@
+clf;
+hold on;
+basename = "data_SG_regular_100";
+plotData([basename,"_4"], "r")
+plotData([basename,"_8"], "g")
+plotData([basename,"_16"], "b")
+plotData([basename,"_32"], "k")
+plotData([basename,"_64"], "c")
